bd968b46a013863ff69906db249d4542d76e7266
madved13 <rsvmicroft@gmail.com>
2018-02-18 20:36:17 +0300

    enh: upd + pyowm

--- main.py
+++ main.py
@@ -4,32 +4,30 @@ import pyowm
 
 bot = telebot.TeleBot(constants.token)
 
-#upd = bot.get_updates()
 
-#last_upd = upd[-1]
-#message_from_user = last_upd.message
-#print(message_from_user)
-"""
-@bot.message_handler(content_types=['text'])
-def handle_text(message):
-    if "триста" in message.text:
-        bot.send_message(message.chat.id, "Отсоси у тракториста!")
-    elif message.text == "куда отправиться?":
-        bot.send_location(message.chat.id, 26.0266276,101.6957962)
-"""
 @bot.message_handler(commands=['pogoda'])
 def handle_pogoda(message):
 
     msg = bot.send_message(message.chat.id, "Какой город Вас интересует?")
     bot.register_next_step_handler(msg,city)
 def city(message):
-    city = message.text
-    owm = pyowm.OWM(constants.owmkey)
-    observation = owm.weather_at_place(city)
-    w = observation.get_weather()
-    temperarure = w.get_temperature('celsius')['temp']
+    try:
+        city = message.text
+        owm = pyowm.OWM(constants.owmkey, language='ru')
+        observation = owm.weather_at_place(city)
+        w = observation.get_weather()
+        temperarure = w.get_temperature('celsius')['temp']
 
-    bot.send_message(message.chat.id,"В городе " + city + " сейчас температура: " + str(temperarure) + " по цельсию")
+        bot.send_message(message.chat.id,"В городе " + city + " сейчас температура: " + str(temperarure) + " по цельсию")
+        bot.send_message(message.chat.id,w.get_detailed_status())
+    except :
+        bot.send_message(message.chat.id, "Не знаю такой город")
 
+@bot.message_handler(content_types=['text'])
+def handle_text(message):
+    if "триста" in message.text:
+        bot.send_message(message.chat.id, "Отсоси у тракториста!")
+    elif message.text == "куда отправиться?":
+        bot.send_location(message.chat.id, 26.0266276,101.6957962)
 
 bot.polling(none_stop=True, interval=0)
